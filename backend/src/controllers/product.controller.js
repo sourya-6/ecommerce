aec7398743724cb08782fd4fa3a89844534b8bac
sourya-6 <saisourya678@gmail.com>
2025-03-07 10:52:15 +0530

    uploading 5 images array using multer

--- backend/src/controllers/product.controller.js
+++ backend/src/controllers/product.controller.js
@@ -8,6 +8,15 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 const createProduct=asyncHandler(async(req,res) =>{
     const {name,price,description,category,brand,images,stock}=req.body;
 
+    if(!name||!price||!description||!category||!brand||!images||!stock){
+        throw new ApiError(400,"All fields are mandatory");
+    }
+    if(!req.files==0 || !req.files.length==0){
+        throw new ApiError(400,"Atleast upload one image");
+    }
+
+    
+
     const product = new Product({
         name:name,
         description:description,
@@ -16,7 +25,16 @@ const createProduct=asyncHandler(async(req,res) =>{
         stock:stock,
         brand:brand,
     })
+
+    await product.save()
+    if(!product){
+        throw new ApiError(400,"Error while creating product")
+    }
     
+    res.status(200)
+    .json(
+         new ApiResponse(200,product,"Product created successfully")
+    )
 })
 export  default createProduct
 
--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -12,4 +12,5 @@ const storage = multer.diskStorage({
   
 export const upload = multer({ 
     storage, 
-})
+    limits:{ fileSize:10*1024*1024 }
+}).array("images",5)
